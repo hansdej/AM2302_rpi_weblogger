3e805f843e5eacf82b6359355ab4a99e8c655a74
Hans de Jonge <j.dejonge@gmail.com>
2017-06-06 07:38:05 +0000

    Objectified sensor logging

--- monitor_AM2302.py
+++ monitor_AM2302.py
@@ -9,108 +9,112 @@ import glob
 import Adafruit_DHT
 
 # global variables
-speriod=(15*60)-1
-pad = '/home/hansdej/Sensorlog/rpi_temp_logger-master'
-dbname=pad+'/am2303log.db'
+speriod     = (15*60)-1
+pad         = '/home/hansdej/Sensorlog/rpi_temp_logger-master'
+pad         = '/home/hansdej/Sensorlog/AM2302_rpi_weblogger'
+dbname      = pad+'/readings.sqlite'
+tablename   = "AM2302"
+columns     = { 'date': 'DATETIME', 'temperature': 'NUMERIC','moisture': 'NUMERIC'}
+# This sqlite database contains a table called "readings"
+# with three columns: timestamp, temp and humid.
 
+# A function to prevent injection.
+def clean_name(some_var):
+    return ''.join(char for char in some_var if char.isalnum())
 
+class SensorLog():
+    """
+    The object that interfaces to the sqlite3 database.
+    """
+    def __init__(self, filename,tablename):
+        self.filename   = filename
+        self.tablename  = tablename
 
-# store the temperature in the database
-def log_temperature_and_humidity(temp,moist):
+    def make_datatable(self, filename,tablename, tables):
+        connection = sqlite3.connect(self.filename)
+        cursor     = connection.cursor()
 
-    conn=sqlite3.connect(dbname)
-    curs=conn.cursor()
+        first   = True
+        for col in columns:
+            if first:
+                # No comma for the first column definition
+                theColumns = ""
+                first   = False
+            else:
+                theColumns += ","
+            theColumns += "\'%s\' \'%s\'"%(clean_name(col),clean_name(column[col]))
+            tablename   = clean_name(tablename)
 
-    curs.execute("INSERT INTO readings values(datetime('now'), (?), (?))", (temp,moist))
-
-    # commit the changes
-    conn.commit()
-
-    conn.close()
-
-
-# display the contents of the database
-def display_data():
-
-    conn=sqlite3.connect(dbname)
-    curs=conn.cursor()
-
-    for row in curs.execute("SELECT * FROM readings"):
-        print str(row[0])+"	"+str(row[1])
-
-    conn.close()
-
-# get temerature
-# returns None on error, or the temperature as a float
-def get_temp(devicefile):
-
-    try:
-        fileobj = open(devicefile,'r')
-        lines = fileobj.readlines()
-        fileobj.close()
-    except:
-        return None
-
-    # get the status from the end of line 1 
-    status = lines[0][-4:-1]
-
-    # is the status is ok, get the temperature from line 2
-    if status=="YES":
-        print status
-        tempstr= lines[1][-6:-1]
-        tempvalue=float(tempstr)/1000
-        print tempvalue
-        return tempvalue
-    else:
-        print "There was an error."
-        return None
-
-def get_temp_and_humid(pin):
-    sensor  = Adafruit_DHT.AM2302
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    if humidity is not None and temperature is not None:
-        temp = float(temperature)
-        humid = float(humidity)
-        return temp, humid
-    else:
-        print "There was an error."
-        return None,None
-
+        cursor.execute("CREATE TABLE %s ( %s )"%(tablename,theColumns))
+        connection.commit()
+        connection.close()
         
 
+    def log_temperature_and_humidity(self, temp, moisture): 
+        """
+        Could undoubtably be written more generic
+        but I will settle with this for now
+        """
 
+        connection = sqlite3.connect(self.filename)
+        cursor     = connection.cursor()
+        table      = clean_name(tablename)
+        cursor.execute("INSERT INTO %s values(datetime('now'), (?), (?))"%table, (temp,moisture))
+        connection.commit()
+        connection.close()
+
+    def store_measurements( self, tablename, data):     
+        """
+        The placeholder for the mentioned, more generic approach.
+        """
+        pass
+
+    def display_data(self):    
+        connection = sqlite3.connect(self.filename)
+        cursor     = connection.cursor()
+        string      = ""
+        for row in cursor.execute("SELECT * FROM %s"%clean_name(self.tablename)):
+            for value in row:
+                string += str(value)+"\t"
+            print(string+"\n")
+        connection.close()    
+            
+class AM2302_sensor:
+    def __init__(self, pin = 4 ):
+        self.pin    =  pin
+        self.sensor = Adafruit_DHT.AM2302
+    def read_temp_and_humid(self): 
+        humidity, temperature   = Adafruit_DHT.read_retry(self.sensor, self.pin)
+        if humidity is not None and temperature is not None:
+            self.humidity       = float(humidity)
+            self.temperature    = float(temperature)
+            return self.temperature,self.humidity 
+        else:
+            print("There was a read error")
+            return None,None
+            
 # main function
 # This is where the program starts 
 def main():
+    
+    sensor = AM2302_sensor()
 
+    temperature, humidity = sensor.read_temp_and_humid()
+    while temperature == None:
+        print("A read error: trying again")
+        temperature, humidity = sensor.read_temp_and_humid(pin)
+        # Maybe we should add a little bit of waiting time here?
 
-#    while True:
+    print ("temperature= %.1f C, humidity: %.1f %s"%(temperature,humidity, r"%") )
 
-    # get the temperature from the device file
-    #temperature get_temp(w1devicefile)
-    pin     = 4
-    temperature, humidity = get_temp_and_humid(pin)
-    if temperature != None:
-        # print ("temperature= %.1f C, humidity: %.1f %s"%(temperature,humidity, r"%") )
-        pass
-    else:
-        # Sometimes reads fail on the first attempt
-        # so we need to retry
-        temperature, humidity = get_temp_and_humid(pin)
-        print ("temperature= %.1f C, humidity: %.1f %s"%(temperature,humidity, r"%") )
-
-        # Store the temperature in the database
-    log_temperature_and_humidity(temperature,humidity)
-
-        # display the contents of the database
-#        display_data()
+    # Store the perature in the database
+    sensorlog = SensorLog(dbname,tablename)
+    sensorlog.log_temperature_and_humidity(temperature,humidity)
+        # lay the contents of the database
+    sensorlog.display_data()
 
 #        time.sleep(speriod)
 
 
 if __name__=="__main__":
     main()
-
-
-
-
